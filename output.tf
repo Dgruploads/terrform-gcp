b9b37fc0052593df08d82bcf3c854f0887c4459a
Dgruploads <41875211+Dgruploads@users.noreply.github.com>
2023-04-23 18:10:08 +0530

    Create output.tf

---
+++ output.tf
@@ -0,0 +1,35 @@
+provider "google" {
+  project = "adept-primacy-383707"
+  region  = "us-east4"
+  zone    = "us-east4-c"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "e2-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    # A default network is created for all GCP projects
+    network = "default"
+    access_config {
+    }
+  }
+}
+
+output "instance_id" {
+  value = google_compute_instance.vm_instance.name
+}
+
+output "instance_name" {
+  value = google_compute_instance.vm_instance.instance_id
+}
+
+output "instance_ip" {
+  value = google_compute_instance.vm_instance.network_interface.0.network_ip
+}
