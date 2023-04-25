e56ab569cf414bc65a0a37d0e0e7e1b009bff104
Dgruploads <41875211+Dgruploads@users.noreply.github.com>
2023-04-25 13:43:14 +0530

    Create main.tf

---
+++ meta-arguments/depends_on/main.tf
@@ -0,0 +1,43 @@
+provider "google" {
+  project = "adept-primacy-383707"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_firewall" "gh-9564-firewall-externalssh" {
+  name    = "gh-9564-firewall-externalssh"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["externalssh"]
+}
+
+resource "google_compute_instance" "dev1" {
+  name         = "gcp-rhel7-dev1-tf"
+  machine_type = "f1-micro"
+  zone         = "us-central1-a"
+  tags         = ["externalssh"]
+
+  boot_disk {
+    initialize_params {
+      image = "centos-cloud/centos-7"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      # Ephemeral
+    }
+  }
+
+  # Ensure firewall rule is provisioned before server, so that SSH doesn't fail.
+  depends_on = ["google_compute_firewall.gh-9564-firewall-externalssh"]
+
+}
