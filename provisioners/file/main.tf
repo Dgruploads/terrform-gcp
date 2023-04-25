a7988592f4b43b89d58dea4dd5b38f8cff8e908f
Dgruploads <41875211+Dgruploads@users.noreply.github.com>
2023-04-25 12:20:18 +0530

    Create main.tf

---
+++ provisioners/file/main.tf
@@ -0,0 +1,41 @@
+provider "google" {
+  project = "adept-primacy-383707"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "e2-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-1404-trusty-v20160406"
+    }
+  }
+
+  network_interface {
+    # A default network is created for all GCP projects
+    network = "default"
+    access_config {
+    }
+  }
+
+  metadata = {
+    ssh-keys = "rabi:${file("./gcloud_instance.pub")}"
+  }
+
+  provisioner "file" {
+    source      = "script.sh"
+    destination = "/tmp/script.sh"
+ 
+    connection {
+      type = "ssh"
+      user = "rabi"
+      private_key = "${file("./gcloud_instance")}"
+      host = self.network_interface[0].access_config[0].nat_ip
+      port = 22
+      agent = false
+    }
+  }
+}
