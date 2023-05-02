e91b6a4b69b1c369414a921d4105510d822aa766
Dgruploads <41875211+Dgruploads@users.noreply.github.com>
2023-05-02 12:17:57 +0530

    Create variables.tf

---
+++ meta-arguments/provider/variables.tf
@@ -0,0 +1,50 @@
+variable "project_name" {
+  type        = string
+  description = "Name of the project to create the resources."
+}
+
+variable "default_region_name" {
+  type        = string
+  description = "name of the region to create the resources"
+}
+
+variable "default_zone_name" {
+  type        = string
+  description = "name of the zone to create the resources"
+}
+
+variable "alternate_region_name" {
+  type        = string
+  description = "name of the region to create the resources"
+}
+
+variable "alternate_zone_name" {
+  type        = string
+  description = "name of the zone to create the resources"
+}
+
+variable "resource_name" {
+  type        = string
+  description = "Name of the google compute engine resource"
+}
+
+
+variable "alternate_resource_name" {
+  type        = string
+  description = "Name of the google compute engine resource"
+}
+
+variable "resource_machine_name" {
+  type        = string
+  description = "Machine type for the google compute engine resource"
+}
+
+variable "resource_image" {
+  type        = string
+  description = "Image to used to launch the machine"
+}
+
+variable "network_type" {
+  type        = string
+  description = "Name of the network to create the resources"
+}
