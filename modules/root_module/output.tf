43ddbf7ae37aac9db174aea06b46745ed0ae3df1
Dgruploads <41875211+Dgruploads@users.noreply.github.com>
2023-05-09 12:35:13 +0530

    Update output.tf

--- modules/root_module/output.tf
+++ modules/root_module/output.tf
@@ -9,15 +9,3 @@ output "instance_name" {
 output "instance_ip" {
   value = google_compute_instance.vm_instance.network_interface.0.network_ip
 }
-
-output "disk_id" {
-  value = google_compute_disk.custom_disk.id
-}
-
-output "disk_name" {
-    value = google_compute_disk.custom_disk.name
-}
-
-output "disk_value" {
-    value = google_compute_disk.custom_disk.image
-}
