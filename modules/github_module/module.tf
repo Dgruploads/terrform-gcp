5ab8fbef01e981ce9fe31759ed4af41ea832eeb6
Dgruploads <41875211+Dgruploads@users.noreply.github.com>
2023-05-09 14:04:11 +0530

    Update module.tf

--- modules/github_module/module.tf
+++ modules/github_module/module.tf
@@ -1,16 +1,12 @@
 module "child_module_example" {
   source                = "github.com/Dgruploads/terrform-gcp/modules/root_module"
+  project_name          = var.project_name
+  region_name           = var.region_name
   resource_name         = var.resource_name
   resource_machine_name = var.resource_machine_name
   resource_image        = var.resource_image
   network_type          = var.network_type
-  disk_name             = var.disk_name
-  disk_type             = var.disk_type
   zone_name             = var.zone_name
-  resource_image        = var.resource_image
-  disk_label            = var.disk_label
-  disk_size             = var.disk_size
-  disk_image            = var.disk_image
   bucket_name           = var.bucket_name
   bucket_location       = var.bucket_location
   bucket_destroy_status = var.bucket_destroy_status
